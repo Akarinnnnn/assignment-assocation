e05463d2071a3af7fda6ec46614de2b6459eca5f
Fa鸽 <43724908+Akarinnnnn@users.noreply.github.com>
2022-11-13 09:06:40 +0800

    进一步配置SpringSecurity

--- src/main/java/oop/clubsv3/SecurityConfigurer.java
+++ src/main/java/oop/clubsv3/SecurityConfigurer.java
@@ -1,12 +1,21 @@
 package oop.clubsv3;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfigurer
@@ -14,25 +23,65 @@ public class SecurityConfigurer
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity security) throws Exception
 	{
-		security.formLogin(o -> {
-			o.failureUrl("/login-failure")
-					.loginPage("/login2")
-					.permitAll(true);
-			
-		});
+		security.formLogin(o -> o.failureUrl("/login-failed")
+										.loginPage("/login2")
+										.permitAll(true));
 		
-		security.authorizeHttpRequests((authz) -> {
-			authz.antMatchers("/login2", "/register").permitAll();
-			authz.anyRequest().authenticated();
-		});
+		security.authorizeHttpRequests((authz) ->
+									   {
+										   authz.antMatchers(
+												   "/login2",
+												   "/register",
+												   "/login-failed"
+										   ).permitAll();
+										   authz.anyRequest().authenticated();
+									   });
 		return security.build();
+	}
+	
+	@Bean
+	public AuthenticationManager configureAuthManager(AuthenticationManagerBuilder builder
+	) throws Exception
+	{
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		builder.authenticationProvider(provider);
+		return builder.build();
 	}
 	
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer()
 	{
-		return web -> {
-			web.ignoring().antMatchers("/login", "/register");
-		};
+		return web -> web.ignoring().antMatchers(
+				"/login",
+				"/register",
+				"/login-failed"
+		);
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsManager(
+			@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+			DataSource src,
+			PasswordEncoder encoder)
+	{
+		UserDetailsManager manager = new JdbcUserDetailsManager(src);
+		if (!manager.userExists("aDmin"))
+		{
+			
+			manager.createUser(User.withUsername("user")
+									   .password(encoder.encode("987654721.33"))
+									   .roles("user")
+									   .build()
+			);
+		}
+		return manager;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		// Argon2PasswordEncoder encoder = new Argon2PasswordEncoder(); classnotfound
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
 	}
 }
