764132a0a60b47824bd24ab0a05eea0e25b1f130
Fa鸽 <43724908+Akarinnnnn@users.noreply.github.com>
2022-11-13 12:01:16 +0800

    旧方法配置spring security

--- src/main/java/oop/clubsv3/SecurityConfigurer.java
+++ src/main/java/oop/clubsv3/SecurityConfigurer.java
@@ -1,38 +1,77 @@
 package oop.clubsv3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import javax.sql.DataSource;
+
+@SuppressWarnings("deprecation")
 @EnableWebSecurity
-public class SecurityConfigurer
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter
 {
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity security) throws Exception
+	private final DataSource dataSource;
+	
+	@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+	public SecurityConfigurer(DataSource dataSource) {this.dataSource = dataSource;}
+	
+	@Override
+	public void configure(HttpSecurity security) throws Exception
 	{
-		security.formLogin(o -> {
+		security.httpBasic().and().formLogin(o -> {
 			o.failureUrl("/login-failure")
-					.loginPage("/login2")
+					// .loginPage("/login2")
 					.permitAll(true);
-			
+			o.defaultSuccessUrl("/index.html");
 		});
+		security.logout();
+		
+		// security.
 		
 		security.authorizeHttpRequests((authz) -> {
 			authz.antMatchers("/login2", "/register").permitAll();
 			authz.anyRequest().authenticated();
 		});
-		return security.build();
+		// return security.build();
 	}
 	
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer()
+	@Override
+	protected void configure(AuthenticationManagerBuilder builder) throws Exception
 	{
-		return web -> {
-			web.ignoring().antMatchers("/login", "/register");
-		};
+		// builder.
+		builder.jdbcAuthentication()
+				.dataSource(dataSource)
+				.passwordEncoder(new BCryptPasswordEncoder())
+				.withUser(User.withUsername("user1").password("987654721.33").roles("user"));
+
+		
+		// return builder.build();
 	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception
+	{
+		// web.ignoring().antMatchers("/login", "/register");
+	}
+	
+	
+	
+	//	@Bean
+//	public WebSecurityCustomizer webSecurityCustomizer()
+//	{
+//		return web -> {
+//
+//		};
+//	}
+	
 }
